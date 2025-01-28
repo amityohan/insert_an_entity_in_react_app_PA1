770fcea14cd6ecc31996de69b48999f64cbb2583
amityohan <amit.yohan@kalvium.community>
2025-01-28 14:37:53 +0530

    finished assignments

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-// src/App.jsx
-
 import React from 'react';
+import { Routes, Route } from 'react-router-dom'; // Import Routes and Route
 import Home from './Home';
-import './App.css'; // Import global styles
+import './App.css';
+import AddBookForm from './components/AddBookForm';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-book" element={<AddBookForm />} />
+      </Routes>
     </div>
   );
 }
